f8cf05698f8d1f018d886a726bdfed4c511d9398
Daniel9974532 <126821421+Daniel9974532@users.noreply.github.com>
2023-11-22 17:35:02 -0500

    Add files via upload

---
+++ src/aplicacion/Aplicacion.java
@@ -0,0 +1,11 @@
+package aplicacion;
+
+public class Aplicacion {
+
+	public static void main(String[] args) {
+		VentanaRegistro miVentanaRegistro;
+		miVentanaRegistro= new VentanaRegistro();
+		miVentanaRegistro.setVisible(true);
+	}
+
+}
